e6b29306475eec1ee5af245b847d1a6d19186d0e
Antonio <antoniovictor200@gmail.com>
2022-05-24 17:00:01 -0300

    adicionando bootstrap, adicionando ssl certificate e só

--- API/Startup.cs
+++ API/Startup.cs
@@ -62,7 +62,7 @@ namespace API
                     x
                         .AllowAnyHeader()
                         .AllowAnyMethod()
-                        .WithOrigins("http://localhost:4200"));
+                        .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
